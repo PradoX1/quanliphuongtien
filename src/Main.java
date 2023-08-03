2f86966991c32363f9129f07087c9dae3e30d203
Nguyễn Hoàng Duy <hsbdu9@gmail.com>
2023-08-03 12:45:18 +0700

    new

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int velocity = 0;
-        int vBus = Velocity.BUS.getSpeedValue();
-        int vTrain = Velocity.TRAIN.getSpeedValue();
-        int vPlane = Velocity.AIRPLANE.getSpeedValue();
         Scanner sc = new Scanner(System.in);
         System.out.println("Chúng tôi có các loại phương tiện sau: " + "\n" + "Bus: bus1, bus2;" + " Train: train1, train2;" + " Plane: plane1, plane2.");
         System.out.println("Mời bạn nhập phương tiện muốn đi : ");
         String vehicle = sc.nextLine().toLowerCase(Locale.ROOT);
         if (vehicle.contains("bus") == true) {
             vehicle = "bus";
-        } else if (vehicle.contains("train")==true) {
-            vehicle="train";
-        } else vehicle="plane";
-        System.out.println("Mời bạn nhập quãng đường muốn đi: S= "+" km");
+        } else if (vehicle.contains("train") == true) {
+            vehicle = "train";
+        } else vehicle = "plane";
+        System.out.println("Mời bạn nhập quãng đường muốn đi: S= " + " km");
         double street = Double.parseDouble(sc.nextLine());
         switch (vehicle) {
             case "bus":
-                velocity = vBus;
+                velocity = Velocity.BUS.getSpeedValue();
                 Bus bus = new Bus(street, velocity);
                 bus.printInfo();
                 break;
             case "train":
-                velocity = vTrain;
+                velocity = Velocity.TRAIN.getSpeedValue();
                 Train train = new Train(street, velocity);
                 train.printInfo();
                 break;
             case "plane":
-                velocity = vPlane;
+                velocity = Velocity.AIRPLANE.getSpeedValue();
                 Plane plane = new Plane(street, velocity);
                 plane.printInfo();
                 break;
